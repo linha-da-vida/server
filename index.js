2a532ce6bd6b1dce77e36dd309224a8251658971
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-02-12 06:15:13 -0300

    build: add express and dotenv

---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.status(200).json({ msg: process.env.SECRET_MSG });
+});
+
+app.listen(3000, () => console.log('uber-ui-clone-backend'));
