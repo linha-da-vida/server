91c6cdb923b57d2bb7bb4f00b27b207601a70aba
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-02-12 06:16:44 -0300

    feat: change route path

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/GCP_API', (req, res) => {
 	res.status(200).json({ msg: process.env.SECRET_MSG });
 });
 
