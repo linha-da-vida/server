0674959d55518d006bc28adcf14a759921aed4a6
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-02-12 09:19:13 -0300

    feat: add template .env file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/GCP_API', (req, res) => {
-	res.status(200).json({ msg: process.env.SECRET_MSG });
+	res.status(200).json({ msg: process.env.GCP_API_KEY });
 });
 
 const PORT = process.env.PORT || 3000;
